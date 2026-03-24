0ea5eeee22246b493940d7d9875f94184e783807
AwonkeMsutu <apecsrealish@gmail.com>
2026-03-24 12:28:42 +0200

    Add files via upload

---
+++ Chapter_2/try-outs/Favourite_number.py
@@ -0,0 +1,5 @@
+favorite_num = 11
+
+message = f"My favorite number is {favorite_num} because it's a prime number!"
+
+print(message)
---
+++ Chapter_2/try-outs/Numbers.py
@@ -0,0 +1,8 @@
+
+print(5 + 3)
+
+print(16 - 8)
+
+print(4 * 2)
+
+print(32 / 4)

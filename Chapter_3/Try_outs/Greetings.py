d2a7365df5813e0edd99b9ae79cd5abaa95fe8ab
AwonkeMsutu <apecsrealish@gmail.com>
2026-03-24 12:51:45 +0200

    Add files via upload

---
+++ Chapter_3/Try_outs/Greetings.py
@@ -0,0 +1,5 @@
+names = ['Joe', 'Andrew', 'Parks', 'Penny']
+print(f"Hey {names[0]}, thanks for helping me with the Python project!")
+print(f"Hey {names[1]}, thanks for helping me with the Python project!")
+print(f"Hey {names[2]}, thanks for helping me with the Python project!")
+print(f"Hey {names[3]}, thanks for helping me with the Python project!")
---
+++ Chapter_3/Try_outs/Your_own_list.py
@@ -0,0 +1,6 @@
+transport = ['Car', 'Walk', 'Cycle', 'Plane', 'Trains']
+print(f"The most convinient mode of travelling for me is by {transport[0]}")
+print(f"At times I don't really mind taking a {transport[1]} once in a while!")
+print(f"Being able to {transport[2]} in my free time is a good way to exercise.")
+print(f"These days travelling via {transport[3]} is getting costly!")
+print(f"Here in the east rand the government must bring back {transport[4]}to help us save on travels")
